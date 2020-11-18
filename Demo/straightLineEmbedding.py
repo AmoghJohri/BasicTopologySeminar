1d78c599c1a5e23a022f3a8a95deced5bbb4f736
amogh <amogh.johri@iiitb.org>
2020-11-19 01:00:14 +0530

    added example

--- Demo/straightLineEmbedding.py
+++ Demo/straightLineEmbedding.py
@@ -47,23 +47,99 @@ def strictlyConvexMapping(G):
     # done
     return
 # main function
+# if __name__ == "__main__":
+#     # defining the vertices
+#     A = Node("A",boundary=True)
+#     B = Node("B",boundary=False)
+#     C = Node("C",boundary=True)
+#     D = Node("D",boundary=True)
+#     E = Node("E", boundary=False)
+#     F = Node("F", boundary=False)
+#     # defining the edges
+#     A.setEdgeList([B, C, D, E])
+#     B.setEdgeList([A, C, E, F])
+#     C.setEdgeList([A, B, D, F])
+#     D.setEdgeList([A, E, C, F])
+#     E.setEdgeList([A, B, D, F])
+#     F.setEdgeList([B, C, D, E])
+#     # defining the graph
+#     G = [A, B, C, D, E, F]
+#     # getting the straight line embedding
+#     strictlyConvexMapping(G)
+#     # plotting the vertices
+#     for each in G:
+#         plt.scatter(each.getValue()[0], each.getValue()[1], 100)
+#     plt.legend([each.getLabel() for each in G], loc = 'upper right')
+#     # plotting the edges
+#     for each in G:
+#         for node in each.getEdgeList():
+#             plt.plot([each.getValue()[0], node.getValue()[0]], [each.getValue()[1], node.getValue()[1]])
+#     # plotting a circle of unit radius
+#     t = np.linspace(0,np.pi*2,100)
+#     plt.plot(np.cos(t), np.sin(t), linewidth=1)
+#     plt.grid()
+#     plt.xlabel("X-Axis")
+#     plt.ylabel("Y-Axis")
+#     plt.title("Straight Line Embedding")
+#     plt.show()
+# main
 if __name__ == "__main__":
     # defining the vertices
-    A = Node("A",boundary=True)
-    B = Node("B",boundary=False)
-    C = Node("C",boundary=True)
-    D = Node("D",boundary=True)
-    E = Node("E", boundary=False)
-    F = Node("F", boundary=False)
+    A = Node("A", boundary=True)
+    B = Node("B", boundary=True)
+    C = Node("C", boundary=True)
+    D = Node("D", boundary=True)
+    E = Node("E", boundary=True)
+    F = Node("F", boundary=True)
+    G = Node("G", boundary=True)
+    H = Node("H", boundary=True)
+    I = Node("I", boundary=True)
+    J = Node("J", boundary=True)
+    K = Node("K", boundary=True)
+    L = Node("L", boundary=True)
+    M = Node("M", boundary=False)
+    N = Node("N", boundary=False)
+    O = Node("O", boundary=False)
+    P = Node("P", boundary=False)
+    Q = Node("Q", boundary=False)
+    R = Node("R", boundary=False)
+    S = Node("S", boundary=False)
+    T = Node("T", boundary=False)
+    U = Node("U", boundary=False)
+    V = Node("V", boundary=False)
+    W = Node("W", boundary=False)
+    X = Node("X", boundary=False)
+    Y = Node("Y", boundary=False)
+    Z = Node("Z", boundary=False)
     # defining the edges
-    A.setEdgeList([B, C, D, E])
-    B.setEdgeList([A, C, E, F])
-    C.setEdgeList([A, B, D, F])
-    D.setEdgeList([A, E, C, F])
-    E.setEdgeList([A, B, D, F])
-    F.setEdgeList([B, C, D, E])
+    A.setEdgeList([B, L, V, M])
+    B.setEdgeList([A, C, M, N])
+    C.setEdgeList([B, N, O, D])
+    D.setEdgeList([C, O, P, E])
+    E.setEdgeList([D, P, F])
+    F.setEdgeList([E, P, Q, G])
+    G.setEdgeList([F, Q, R, H])
+    H.setEdgeList([G, R, S, I])
+    I.setEdgeList([H, S, T, J])
+    J.setEdgeList([I, T, U, K])
+    K.setEdgeList([J, U, L])
+    L.setEdgeList([K, U, V, A])
+    M.setEdgeList([A, B, V, W, X, N])
+    N.setEdgeList([B, C, M, X, O])
+    O.setEdgeList([C, D, P, X, N])
+    P.setEdgeList([D, E, F, Q, X, O])
+    Q.setEdgeList([P, F, G, R, S, X])
+    R.setEdgeList([G, H, Q, S])
+    S.setEdgeList([Q, R, H, I, T, X])
+    T.setEdgeList([X, S, I, J, U, V, Z, Y])
+    U.setEdgeList([T, J, K, L, V])
+    V.setEdgeList([T, U, L, A, M, W, Z])
+    W.setEdgeList([M, X, Y, Z, V])
+    X.setEdgeList([M, N, O, P, Q, S, T, Y, W])
+    Y.setEdgeList([W, Z, X, T])
+    Z.setEdgeList([Y, T, V, W])
     # defining the graph
-    G = [A, B, C, D, E, F]
+    G = [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z]
     # getting the straight line embedding
     strictlyConvexMapping(G)
     # plotting the vertices
@@ -76,7 +152,7 @@ if __name__ == "__main__":
             plt.plot([each.getValue()[0], node.getValue()[0]], [each.getValue()[1], node.getValue()[1]])
     # plotting a circle of unit radius
     t = np.linspace(0,np.pi*2,100)
-    plt.plot(np.cos(t), np.sin(t), linewidth=1)
+    plt.plot(np.cos(t), np.sin(t), linewidth=1, color='k')
     plt.grid()
     plt.xlabel("X-Axis")
     plt.ylabel("Y-Axis")
